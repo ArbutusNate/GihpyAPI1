0894a387ebf4bb5c69605380842d4efc3cc6a3d0
ArbutusNate <arbutusnate@gmail.com>
2017-06-01 18:30:57 -0700

    Final Commit

---
+++ javascript/javascript.js
@@ -0,0 +1,55 @@
+var btnarray = ["30 Rock", "Comedy Bang! Bang!", "Community", "Always Sunny", "Parks and Rec", "MST3K", "Broad City"];
+function addbuttons(){
+    $(".buttonzone").empty();
+    for (var i = 0; i < btnarray.length; i++) {
+      var addbtn = $("<button>");
+      addbtn
+        .addClass("btn")
+        .attr("data-name", btnarray[i])
+        .text(btnarray[i])
+        .appendTo(".buttonzone");
+    }
+};
+
+addbuttons();
+// Search/AJAX call and Image Creation
+  $(document).on("click", ".btn", function search(){
+      $(".gifzone").empty();
+      var currentsearch = $(this).attr("data-name");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + currentsearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).done(function(response){
+        console.log(response);
+        var fixedimage;
+        var movinggif;
+        for(i = 0; i < response.data.length; i++){
+          fixedimage = response.data[i].images.fixed_width_still.url;
+          movinggif = response.data[i].images.fixed_width.url;
+          var imgframe = $("<div class='imgframe col-md-2'>");
+          imgframe
+            .attr("data-stillimg", fixedimage)
+            .append("<img src='" + fixedimage + "' class='imgbtn' otherstate='" + movinggif + "'>")
+            .append("<div class='rating'> Rating: " + (response.data[i].rating).toUpperCase() + "</div>")
+            .appendTo($(".gifzone"));
+        };
+      })
+  });
+// Turn Gifs On and Off
+  $(document).on("click", ".imgbtn", function(){
+    console.log("clicked");
+    var srcswitch = $(this).attr("otherstate");
+    var flavorsaver = $(this)[0].src;
+    $(this)[0].src = srcswitch;
+    $(this).attr("otherstate", flavorsaver);
+
+  })
+// Add Buttons to Array
+  $(".usersubmit").on("click", function searchbutton(){
+    debugger;
+    var search = $(".userinput").val().trim().toUpperCase();
+    btnarray
+      .push(search)
+    addbuttons();
+  });
